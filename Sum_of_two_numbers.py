ed58c148c2b08ac9ce74cb52c7f65cb612fa02ac
RAJ <48962069+rajcb987@users.noreply.github.com>
2024-06-16 12:14:02 +0530

    Create Sum_of_two_numbers.py
    Addition of two no.s using normal and Lambda Function

---
+++ Sum_of_two_numbers.py
@@ -0,0 +1,25 @@
+'''
+Using Lambda Function:
+  
+sumFunction = lambda x , y : x + y
+a = sumFunction(2,3)
+print(f"Sum is : {a}")
+  
+// Directly calling in Print:
+
+sumFunction = lambda x , y : x + y
+print(f"Sum is :  {sumFunction(2,4)}")
+
+
+'''
+''' Adding using nnormal function'''
+def sumFunction(x,y):
+    a = x + y
+    return a
+
+result = sumFunction(2,4)
+print(f"Sum is : {result}")
+
+
+
+
