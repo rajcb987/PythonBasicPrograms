aa2ec4dd584845e4719c62a95c948e8e5f4d740f
RAJ <48962069+rajcb987@users.noreply.github.com>
2024-06-18 12:13:27 +0530

    Create listElmRemoval.py
    Removing elements from a list

---
+++ listElmRemoval.py
@@ -0,0 +1,7 @@
+lst = ['Iris', 'Orchids', 'Rose', 'Lavender',
+	'Lily', 'Carnations']
+print("Original List is :", lst)
+
+# using remove()
+lst.remove('Orchids')
+print("After deleting the item :", lst)
